916e321fb5383471ca505f4d6e6cf89cc1062fea
leochen <event10342012@gmail.com>
2021-04-03 09:57:56 +0800

    feat: object interact

--- python/10_interact.py
+++ python/10_interact.py
@@ -1,6 +1,6 @@
-class Phone:
+class Product:
     def __init__(self, brand: str, type_no: str, price: int):
-        self.branch = brand
+        self.brand = brand
         self.type_no = type_no
         self.price = price
 
@@ -9,7 +9,7 @@ class Phone:
         print(message, 'from', self.type_no)
 
 
-class PhoneStore:
+class Store:
     def __init__(self, name: str, cash: int, income: int = 0):
         self.name = name
         self.income = income
@@ -17,10 +17,12 @@ class PhoneStore:
 
         self.stock = []
 
-    def purchase(self, number: int, phone: Phone):
+    def purchase(self, number: int, product: Product):
         for i in range(number):
-            self.stock.append(phone)
-            self.cash -= phone.price
+            self.stock.append(product)
+            self.cash -= product.price
+            print(f'Purchase {product.brand}-{product.type_no}')
+        print(f'This is {self.cash} left.')
 
 
 class People:
@@ -31,17 +33,17 @@ class People:
         self.cash = cash
         self.phone = None
 
-    def buy_phone(self, store: PhoneStore):
+    def buy(self, store: Store):
         if len(store.stock) > 0:
             phone = store.stock.pop()
-            if phone.price < self.cash:
+            if phone.price <= self.cash:
                 self.phone = phone
                 self.cash -= phone.price
             else:
                 store.stock.append(phone)
                 print('You cannot afford it!!!')
         else:
-            print('Sell out!!!')
+            print('sold out!!!')
 
     def call(self, people, message):
         if self.phone:
@@ -51,14 +53,16 @@ class People:
 
 
 if __name__ == '__main__':
-    leo = People(name='Leo', age=27, gender='m', cash=100)
+    # assume store sell only one product in this world
+
+    leo = People(name='Leo', age=27, gender='m', cash=150)
     john = People(name='John', age=25, gender='m', cash=150)
 
-    iphone = Phone(brand='Iphone', type_no='IphoneX', price=150)
+    iphone = Product(brand='Iphone', type_no='IphoneX', price=150)
 
-    iphone_store = PhoneStore(name='IphoneStore', cash=1000, income=0)
+    iphone_store = Store(name='IphoneStore', cash=1000, income=0)
     iphone_store.purchase(5, iphone)
 
-    leo.buy_phone(iphone_store)
+    leo.buy(iphone_store)
 
     leo.call(john, 'I bought a wonderful phone.')
